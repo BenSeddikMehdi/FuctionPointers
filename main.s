29329c8467a0315fcfd23245d1d515cd2b08e3ad
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-25 19:16:15 +0100

    Adding Assembler file by converting the main.c into  main.s

---
+++ main.s
@@ -0,0 +1,186 @@
+	.file	"main.c"
+	.text
+	.globl	_add
+	.def	_add;	.scl	2;	.type	32;	.endef
+_add:
+LFB10:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	8(%ebp), %edx
+	movl	12(%ebp), %eax
+	addl	%edx, %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE10:
+	.globl	_multiply
+	.def	_multiply;	.scl	2;	.type	32;	.endef
+_multiply:
+LFB11:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	8(%ebp), %eax
+	imull	12(%ebp), %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE11:
+	.globl	_do_operation
+	.def	_do_operation;	.scl	2;	.type	32;	.endef
+_do_operation:
+LFB12:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	16(%ebp), %eax
+	movl	%eax, 4(%esp)
+	movl	12(%ebp), %eax
+	movl	%eax, (%esp)
+	movl	8(%ebp), %eax
+	call	*%eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE12:
+	.globl	_do_ptr
+	.def	_do_ptr;	.scl	2;	.type	32;	.endef
+_do_ptr:
+LFB13:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	16(%ebp), %eax
+	movl	%eax, 4(%esp)
+	movl	12(%ebp), %eax
+	movl	%eax, (%esp)
+	movl	8(%ebp), %eax
+	call	*%eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE13:
+	.section .rdata,"dr"
+LC0:
+	.ascii "%d : %d\12\0"
+	.text
+	.globl	_print_ints
+	.def	_print_ints;	.scl	2;	.type	32;	.endef
+_print_ints:
+LFB14:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$40, %esp
+	leal	12(%ebp), %eax
+	movl	%eax, -20(%ebp)
+	movl	$0, -12(%ebp)
+	jmp	L10
+L11:
+	movl	-20(%ebp), %eax
+	leal	4(%eax), %edx
+	movl	%edx, -20(%ebp)
+	movl	(%eax), %eax
+	movl	%eax, -16(%ebp)
+	movl	-16(%ebp), %eax
+	movl	%eax, 8(%esp)
+	movl	-12(%ebp), %eax
+	movl	%eax, 4(%esp)
+	movl	$LC0, (%esp)
+	call	_printf
+	addl	$1, -12(%ebp)
+L10:
+	movl	-12(%ebp), %eax
+	cmpl	8(%ebp), %eax
+	jl	L11
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE14:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+	.align 4
+LC1:
+	.ascii "a = %d\12b = %d\12a_ptr = %d\12b_ptr = %d\12\0"
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB15:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$48, %esp
+	call	___main
+	movl	$6, 8(%esp)
+	movl	$5, 4(%esp)
+	movl	$_add, (%esp)
+	call	_do_operation
+	movl	%eax, 44(%esp)
+	movl	$4, 8(%esp)
+	movl	$7, 4(%esp)
+	movl	$_multiply, (%esp)
+	call	_do_operation
+	movl	%eax, 40(%esp)
+	movl	$6, 8(%esp)
+	movl	$5, 4(%esp)
+	movl	$_add, (%esp)
+	call	_do_ptr
+	movl	%eax, 36(%esp)
+	movl	$4, 8(%esp)
+	movl	$7, 4(%esp)
+	movl	$_multiply, (%esp)
+	call	_do_ptr
+	movl	%eax, 32(%esp)
+	movl	32(%esp), %eax
+	movl	%eax, 16(%esp)
+	movl	36(%esp), %eax
+	movl	%eax, 12(%esp)
+	movl	40(%esp), %eax
+	movl	%eax, 8(%esp)
+	movl	44(%esp), %eax
+	movl	%eax, 4(%esp)
+	movl	$LC1, (%esp)
+	call	_printf
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE15:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
+	.def	_printf;	.scl	2;	.type	32;	.endef

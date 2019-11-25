f2c4e996e00fa27e2efda64311c61e6031afdf48
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-25 02:02:22 +0100

    Working on functions that have lists as arguments

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main() {
     printf("a = %d\nb = %d\na_ptr = %d\nb_ptr = %d\n", a, b, a_ptr, b_ptr);*/
     print_ints(5, 34, 45, 5, 4, 8);
     print_ints(3, 4, 5, 6);
-    print_ints(4, 11, 19, 20, 37);
+    print_ints(4, 34, 19, 20, 37);
+
     return 0;
 }

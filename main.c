eea590691fbe6ea89ba7a33655f47733ef087c35
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-26 23:16:54 +0100

    Working on Function with List Arguments

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ int main() {
     int b = do_operation(multiply, 7, 4);
     int a_ptr = do_ptr(add, 5, 6);
     int b_ptr = do_ptr(multiply, 7, 4);
-    printf("a = %d\nb = %d\na_ptr = %d\nb_ptr = %d\n", c, b, a_ptr, b_ptr);
+    printf("a = %d\nb = %d\na_ptr = %d\nb_ptr = %d\n"
+            , c, b, a_ptr, b_ptr);
 /*    print_ints(3, 34, 45, 5);
     print_ints(2, 0, -2);
     print_ints(6, 34, 19, 20, 37, 6, 6);*/

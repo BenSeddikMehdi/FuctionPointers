5c3478b12c75d0f605c8482803c7c1aa2f73e444
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-26 19:54:32 +0100

    Adding Assembler file by converting the main.c into  main.s

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include "FunctionReturnsString.h"
 
 int main() {
+
+/*    int c = do_operation(add, 6, -9);
     int a = do_operation(add, 5, 6);
     int b = do_operation(multiply, 7, 4);
     int a_ptr = do_ptr(add, 5, 6);
     int b_ptr = do_ptr(multiply, 7, 4);
-    printf("a = %d\nb = %d\na_ptr = %d\nb_ptr = %d\n", a, b, a_ptr, b_ptr);
+    printf("a = %d\nb = %d\na_ptr = %d\nb_ptr = %d\n", c, b, a_ptr, b_ptr);*/
 /*    print_ints(5, 34, 45, 5, 4, 8);
     print_ints(3, 4, 5, 6);
     print_ints(4, 34, 19, 20, 37);*/

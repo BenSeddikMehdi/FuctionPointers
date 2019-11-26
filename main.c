e661e252703109ac02239261979d9e6fe939217e
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-26 23:50:45 +0100

    Working on Function with List Arguments

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 #include "FunctionPointers.h"
 
 int main() {
-    int c = do_operation(add, 6, -9);
-    int a = do_operation(add, 5, 6);
+    int a = do_operation(add, 6, -9);
     int b = do_operation(multiply, 7, 4);
     int a_ptr = do_ptr(add, 5, 6);
     int b_ptr = do_ptr(multiply, 7, 4);
-    printf("a = %d\nb = %d\na_ptr = %d\nb_ptr = %d\n"
-            , c, b, a_ptr, b_ptr);
+    printf("a = %d\nb = %d\na_ptr = %d\nb_ptr = %d\n",
+            a, b, a_ptr, b_ptr);
 
 
     return 0;

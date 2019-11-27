e8b4a82807fd07882d28a07c7a8c495de0cbd8aa
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-27 23:37:52 +0100

    Working on Function pointers

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main() {
     int a = do_operation(add, 6, -9);
     int b = do_operation(multiply, 7, 4);
     int a_ptr = do_ptr(add, 5, 6);
-    int b_ptr = do_ptr(multiply, 7, 4);
+    int b_ptr = do_ptr(multiply, 9, 4);
     printf("a = %d\nb = %d\na_ptr = %d\nb_ptr = %d\n",
             a, b, a_ptr, b_ptr);
 

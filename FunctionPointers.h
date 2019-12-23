459a5272ec8b5bc827dc4f7e05785995322c3c2a
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-23 15:50:42 +0100

    Working on Function pointers

--- FunctionPointers.h
+++ FunctionPointers.h
@@ -5,16 +5,5 @@
 #ifndef FUCTIONPOINTERS_FUNCTIONPOINTERS_H
 #define FUCTIONPOINTERS_FUNCTIONPOINTERS_H
 
-typedef int (*operation_ptr) (int, int);
 
-int add(int a, int b) { return a + b;}
-int multiply(int a, int b) { return a * b;}
-
-int do_operation(int (*op)(int, int), int x, int y) {
-    return op(x, y);
-}
-int do_ptr(operation_ptr op_ptr, int x, int y) {
-    return op_ptr(x, y);
-}
-int externVar = 12;
 #endif //FUCTIONPOINTERS_FUNCTIONPOINTERS_H
--- Function_Pointers.h
+++ Function_Pointers.h
@@ -5,4 +5,13 @@
 #ifndef FUCTIONPOINTERS_FUNCTION_POINTERS_H
 #define FUCTIONPOINTERS_FUNCTION_POINTERS_H
 
+// Add function
+int add(int a, int b);
+
+// Multiply function
+int multiply(int a, int b);
+
+// Function Pointer:
+int do_operation(int (*op)(int, int), int x, int y);
+
 #endif //FUCTIONPOINTERS_FUNCTION_POINTERS_H
